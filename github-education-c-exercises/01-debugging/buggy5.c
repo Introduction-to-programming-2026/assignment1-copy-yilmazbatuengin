0f4e09c3a77e20687d9b925f8cbc34b7cd305b51
yilmazbatuengin <sns6267f9s@privaterelay.appleid.com>
2026-04-30 16:27:24 +0200

    buggy5 fixed

--- github-education-c-exercises/01-debugging/buggy5.c
+++ github-education-c-exercises/01-debugging/buggy5.c
@@ -1,18 +1,20 @@
-// Buggy example for debug50
-#include <cs50.h>
 #include <stdio.h>
 
 void print_column(int height);
 
 int main(void)
 {
-    int h = get_int("Height: ");
+    int h;
+
+    printf("Height: ");
+    scanf("%d", &h);
+
     print_column(h);
 }
 
 void print_column(int height)
 {
-    for (int i = 0; i <= height; i++)
+    for (int i = 0; i < height; i++)
     {
         printf("#\n");
     }
